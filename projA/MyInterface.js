899b5a80ef750d234d2e1a7fcf88f6bd2ad6bf3c
Márcia Teixeira <marcia309@hotmail.com>
2019-04-17 15:12:38 +0100

    Finished sun and moon lights

--- projA/MyInterface.js
+++ projA/MyInterface.js
@@ -20,12 +20,6 @@ class MyInterface extends CGFinterface {
        
         this.gui.add(this.scene, 'mode', ['Day', 'Night']).name("Mode");
         
-        var f0 = this.gui.addFolder('Light 0 ');
-        f0.add(this.scene.lights[0], 'enabled').name("Enabled");
-        
-        var f1 = this.gui.addFolder('Light 1 ');
-        f1.add(this.scene.lights[1], 'enabled').name("Enabled");
-        
         var obj = this;
 
         return true;
